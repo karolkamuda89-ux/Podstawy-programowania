f0fd35855ccafa5a4a5a196d5b1eb2b1342c2271
s-A301-04 <s-A301-04@CAMPUS.UEK.KRAKOW.PL>
2025-10-25 09:57:45 +0200

    podstawy programowania

---
+++ 3.3.py
@@ -0,0 +1,7 @@
+x = 7
+y = 34
+print('numbers before swap:', x , y)
+z = x
+x = y 
+y = z
+print('numbers after swap', x , y)
---
+++ 3.4.py
@@ -0,0 +1,3 @@
+speed_kmh = 70 
+speed_ms = speed_kmh / 3.6
+print("speedkmh :", speed_kmh,  "speedms:", speed_ms )
